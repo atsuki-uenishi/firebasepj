7f9ae6cfd7c913ae851a7be08c209363f7113fef
Atsuki Uenishi <cacao3725@gmail.com>
2021-06-24 12:30:19 +0900

    ログイン機能の実装

---
+++ pages/logout.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="register">
+        <button @click="logout">ログアウト</button>
+        <br>
+        <nuxt-link to="/">戻る</nuxt-link>
+    </div>
+</template>
+
+<script>
+import firebase from '~/plugins/firebase'
+export default {
+    methods: {
+        logout() {
+            firebase
+                .auth()
+                .signOut()
+                .then(() => {
+                    alert('ログアウトが完了しました')
+                    this.$router.replace('/')
+                })
+        }
+    }
+}
+</script>
